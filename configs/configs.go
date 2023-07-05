462f14672f66a5c1a51563c5aac0ffcf72cab0cd
ali <ali.sdfi.dev@gmail.com>
2023-07-06 02:34:46 +0330

    fix: some fixes for unmarshalling process

--- configs/configs.go
+++ configs/configs.go
@@ -3,34 +3,42 @@ package configs
 var Config Configuration
 
 type ServerConfiguration struct {
-	Host     string
-	GRPCPort string
-	HTTPPort string
+	Host     string `mapstructure:"host"`
+	GRPCPort string `mapstructure:"grpcport"`
+	HTTPPort string `mapstructure:"httpport"`
 }
 
 type DatabaseConfiguration struct {
-	Host                  string
-	Port                  int
-	User                  string
-	Password              string
-	DB                    string
-	ConnectionMaxLifetime int
-	MaxIdleConnections    int
-	MaxOpenConnections    int
+	Host                  string `mapstructure:"host"`
+	Port                  int    `mapstructure:"port"`
+	User                  string `mapstructure:"user"`
+	Password              string `mapstructure:"password"`
+	DB                    string `mapstructure:"db"`
+	ConnectionMaxLifetime int    `mapstructure:"connectionMaxLifetime"`
+	MaxIdleConnections    int    `mapstructure:"MaxIdleConnections"`
+	MaxOpenConnections    int    `mapstructure:"MaxOpenConnections"`
+}
+
+type CacheConfiguration struct {
+	Client   string `mapstructure:"client"`
+	Host     string `mapstructure:"host"`
+	Port     string `mapstructure:"port"`
+	Password string `mapstructure:"password"`
 }
 
 type Credential struct {
-	TokenSecret string
+	TokenSecret string `mapstructure:"tokensecret"`
 }
 
 type RateLimitConfiguration struct {
-	Name              string
-	RequestsPerSecond int
+	RPCName           string `mapstructure:"rpcName"`
+	RequestsPerSecond int    `mapstructure:"requestsPerSecond"`
 }
 
 type Configuration struct {
-	Server     ServerConfiguration
-	Database   DatabaseConfiguration
-	Credential Credential
-	RateLimits []RateLimitConfiguration
+	Server     ServerConfiguration      `mapstructure:"server"`
+	Database   DatabaseConfiguration    `mapstructure:"database"`
+	Cache      CacheConfiguration       `mapstructure:"cache"`
+	Credential Credential               `mapstructure:"credential"`
+	RateLimits []RateLimitConfiguration `mapstructure:"ratelimit"`
 }
