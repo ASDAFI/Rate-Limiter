30ee1961ce59716a286a8c5bf7d8a3c4d7c560c7
ali <ali.sdfi.dev@gmail.com>
2023-07-06 03:39:07 +0330

    update: setup configs (connecting to providers)

--- src/infrastructure/cli/config.go
+++ src/infrastructure/cli/config.go
@@ -4,6 +4,8 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"service/configs"
+	"service/src/infrastructure/cache"
+	"service/src/infrastructure/db"
 )
 
 var configFile string
@@ -26,21 +28,25 @@ func initConfig() {
 		log.Fatal("There is no token secret in config file\n")
 	}
 
-	//dbProvider, err := infrastructure.CreateDBProvider(configs.Config.Database)
-	//if err != nil {
-	//	log.Fatalf("Fatal error on create db: %s \n", err)
-	//}
-	//infrastructure.PostgresDBProvider = dbProvider
-
-	log.Info("Configuration initialized!")
-
 	if configs.Config.Credential.TokenSecret == "" {
 		log.Fatal("There is no token secret in config file\n")
 	}
 
-	//dbProvider, err := infrastructure.CreateDBProvider(configs.Config.Database)
-	//if err != nil {
-	//	log.Fatalf("Fatal error on create db: %s \n", err)
-	//}
-	//infrastructure.PostgresDBProvider = dbProvider
+	log.Info("Configuration initialized!")
+
+}
+
+func setupConfig() {
+	redisProvider, err := cache.CreateRedisCacheProvider(configs.Config.Cache)
+	if err != nil {
+		log.Fatal("Error while creating redis cache provider: ", err)
+	}
+	cache.RedisCacheProvider = redisProvider
+
+	postgresProvider, err := db.CreateDBProvider(configs.Config.Database)
+	if err != nil {
+		log.Fatal("an Error has been occured while creating postgres db provider: ", err)
+	}
+	db.PostgresDBProvider = postgresProvider
+
 }
